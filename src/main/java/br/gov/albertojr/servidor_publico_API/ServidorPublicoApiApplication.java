f773e46eb56aee382cb46dd2ecfb56cc0f5092b8
Alberto-debug <albertojunior848423803@gmail.com>
2026-01-27 19:02:56 +0300

    first commit

---
+++ src/main/java/br/gov/albertojr/servidor_publico_API/ServidorPublicoApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.albertojr.servidor_publico_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorPublicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorPublicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/albertojr/servidor_publico_API/ServidorPublicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.albertojr.servidor_publico_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorPublicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
