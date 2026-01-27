b6ed5a349fa44ca4d83cccc0c5334ba47be08ca3
Alberto-debug <albertojunior848423803@gmail.com>
2026-01-27 20:08:37 +0300

    config changes

---
+++ src/main/java/br/gov/albertojr/servidor_publico_API/Home/HomeController.java
@@ -0,0 +1,26 @@
+package br.gov.albertojr.servidor_publico_API.Home;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, Object> HomePage(){
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", "Servidor Publico");
+        response.put("version", "1.0.0");
+        response.put("status", "running");
+
+
+        return response;
+
+    }
+}
