ecbec29391f751d4cc29124b3d09d49419c1609e
Alberto <alberto@example.com>
2026-02-18 12:07:50 +0300

    fix on EnderecoBusiness

--- src/main/java/br/gov/albertojr/servidor_publico_API/config/Cors.java
+++ src/main/java/br/gov/albertojr/servidor_publico_API/config/Cors.java
@@ -13,7 +13,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://albertojunior.me")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
